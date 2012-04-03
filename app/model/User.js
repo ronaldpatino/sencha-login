d3f3638099577fcd2bf3670c8a40562f3eb5aca1
ronaldpatino <ronaldpatino@gmail.com>
2012-04-03 15:09:03 -0500

    Added User model

---
+++ app/model/User.js
@@ -0,0 +1,12 @@
+Ext.define('ime.model.User', {
+    extend: 'Ext.data.Model',
+    
+    config: {
+        fields: [
+            {name: 'id', type: 'int'},
+            {name: 'username', type: 'auto'},
+			{name: 'password', type: 'auto'},
+			{name: 'nombre', type: 'auto'}
+        ]
+    }
+});
